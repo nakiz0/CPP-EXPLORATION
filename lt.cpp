97a4739f3d2d41eebc64a0d06b4f73208841ac96
nakiz0 <amritxtamu132@gmail.com>
2026-02-05 20:22:26 +0530

    progression

---
+++ lt.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main(){
+     int x = 10;
+     const int y = x;
+     std::cout << x << std::endl;
+     std::cout << y << std::endl;
+}
---
+++ strt.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(){
+
+  const double pi= 3.141;
+  cout<<"ENTER RADIOUS: ";
+  double radious;
+  cin>>radious;
+  double area=pi*radious*radious;
+  cout<<"AREA OF A SQUARE : "<<area<<endl;
+
+
+
+}
