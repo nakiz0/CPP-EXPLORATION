9c9250db543dd8b2f187e0d5d7e8a542251f153d
nakiz0 <amritxtamu132@gmail.com>
2026-02-08 20:29:54 +0530

    affermative

--- reverse_array.cpp
+++ reverse_array.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
-
+/*
 //REVERSING THE ARRAY
 void reverse(int arr[],int sz) {
 
@@ -23,5 +23,30 @@ int main() {
         cout<<arr[i]<<" ";
     }
 }
+
+*/
+//REVERSING THE HALF ARRAY
+
+
+void h_rev(int arr[],int n) {
+
+    for (int i=0;i<n/4;i++) {
+        int temp=arr[n/2-1-i];
+        arr[n/2-1-i]=arr[i];
+        arr[i]=temp;
+    }
+}
+
+int main() {
+    int arr[]={5,2,3,1,4,6};
+    int sz=6;
+    h_rev(arr,sz);
+    for (int i=0;i<sz;i++) {
+        cout<<arr[i]<<" ";
+    }
+}
+
+
+
 
 
