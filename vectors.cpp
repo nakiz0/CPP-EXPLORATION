65b15c69cf78574271bbc842ab027d74eed66a8a
nakiz0 <amritxtamu132@gmail.com>
2026-02-11 22:33:31 +0530

    added

--- vectors.cpp
+++ vectors.cpp
@@ -5,6 +5,8 @@ using namespace std;
 int main() {
     vector<char> ch={'a','b','c','d','e'};
     vector<int> vec;
+
+    //SIZE
     cout<<"size = " << ch.size()<<endl;
     cout<<"size before push_back = " << vec.size()<<endl;
     vec.push_back(25);
@@ -17,11 +19,16 @@ int main() {
     for (int vall:vec) {
         cout<<"after push_back = "<<vall<<endl;
     }
+
+    //POP_BA
     vec.pop_back();
     for (int vall:vec) {
         cout<<"after pop_back = "<<vall<<endl;
     }
+    //FRONT VA
     cout<<"front val ="<<vec.front()<<endl;
+
+    //LAST V
     cout<<"front val ="<<vec.back()<<endl;
     vector<int> ba(5,1);
     cout<<vec.at(1)<<endl;
