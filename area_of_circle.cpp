7ab06180f3da598302d50fce253348e74e180b70
nakiz0 <amritxtamu132@gmail.com>
2026-02-11 22:24:32 +0530

    added

--- area_of_circle.cpp
+++ area_of_circle.cpp
@@ -13,3 +13,32 @@ int main(){
 
 
 }
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int result=0;
+    char eval;
+    int n;
+    cout<<"\t---------CALUCULATOR----------";
+    cout<<"EVALUATORS\n1. +\n2. -\n3. *\n4. /"<<endl;
+    cout<<"enter the evaluator"<<endl;
+    cin>>eval;
+    cout<<"ENTER THE NUMBER OF VALUES YOU WANNA CALCULATE"<<endl;
+    cin>>n;
+    cout<<"ENTER THE NUMBERS"<<endl;
+    int val[n];
+    for (int i=0;i<n;i++) {
+        cin>>val[i];
+    }
+    if (eval=='+') {
+        for (int i=0;i<n;i++) {
+            result=result+ val[i];
+
+        }
+        cout<<"OUTPUT: "<<result<<endl;
+
+    }
+return 0;
+}
---
+++ vectors.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    vector<char> ch={'a','b','c','d','e'};
+    vector<int> vec;
+    cout<<"size = " << ch.size()<<endl;
+    cout<<"size before push_back = " << vec.size()<<endl;
+    vec.push_back(25);
+    vec.push_back(26);
+    vec.push_back(27);
+    vec.push_back(28);
+    cout<<"size after push_back= " << vec.size()<<endl;
+    for (int vall:vec) {
+        cout<<"after push_back = "<<vall<<endl;
+    }
+    vec.pop_back();
+    for (int vall:vec) {
+        cout<<"after pop_back = "<<vall<<endl;
+    }
+    cout<<"front val ="<<vec.front()<<endl;
+    cout<<"front val ="<<vec.back()<<endl;
+    vector<int> ba(5,1);
+    cout<<vec.at(1)<<endl;
+    for (char val :ch){
+        cout << val<<endl;
+    }
+
+    return 0;
+
+
+
+}
