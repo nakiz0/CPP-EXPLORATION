35b18115fa1dc3c5acff6ea75efb17a63ca708f8
nakiz0 <amritxtamu132@gmail.com>
2026-02-15 20:58:29 +0530

    ll.cpp

--- ll.cpp
+++ ll.cpp
@@ -13,11 +13,29 @@ class Node {
 
 };
 
+void insertAtHead(Node* &head,int val) {
 
+    Node* new_node=new Node(val);
+    new_node->next=head;
+    head=new_node;
+}
+
+void display(Node* head) {
+
+    Node* temp=head;
+    while (temp!=NULL) {
+        cout<<temp->val<<"->";
+        temp= temp->next;
+
+    }
+    cout<<"NULL"<<endl;
+}
 
 int main() {
 
-    Node* n=new Node(1);
-    cout<<n->val<<" "<<n->next<<endl;
-    return 0;
+    Node* head = NULL;
+    insertAtHead(head, 2);
+    display(head);
+    insertAtHead(head,1);
+    display(head);
 }
